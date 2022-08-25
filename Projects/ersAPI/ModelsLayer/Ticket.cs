4efc52b7dff2609ec79c493c33ed43499216b1b6
Jonathan Kim <jonathan241@revature.net>
2022-08-25 08:21:29 -0700

    try2

---
+++ Projects/ersAPI/ModelsLayer/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer
+{
+    public class Ticket
+    {
+        public Ticket(Guid ticketID, Guid fK_Employee, string description, decimal amount, int status)
+        {
+            TicketID = ticketID;
+            FK_Employee = fK_Employee;
+            Description = description;
+            Amount = amount;
+            Status = status;
+        }
+
+        public Guid TicketID { get; set; }
+        public Guid FK_Employee { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public int Status { get; set; }
+    }
+}
