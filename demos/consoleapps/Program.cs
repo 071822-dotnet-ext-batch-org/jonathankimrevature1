9ac3c777f582e145f3a88db5808af35012102de9
Jonathan Kim <jonathan241@revature.net>
2022-09-06 07:38:17 -0700

    changes2

--- demos/consoleapps/Program.cs
+++ demos/consoleapps/Program.cs
@@ -8,14 +8,7 @@ namespace Program
 {    
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] values = {21,35,97,364,3212,3125};
-
-            int y = 97;
-
-            Console.WriteLine(values(97));
-        }
+        
     }
 }
 
