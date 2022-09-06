9162db26ff8ad9e4dd560afa0855f4261d77d09a
Jonathan Kim <jonathan241@revature.net>
2022-09-06 07:35:03 -0700

    html demo changes

--- demos/consoleapps/Program.cs
+++ demos/consoleapps/Program.cs
@@ -4,26 +4,18 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace MyApplication
-{
+namespace Program
+{    
     class Program
     {
         static void Main(string[] args)
         {
-            using(var source = new HttpClient())
-            {
-                var endpoint = new Uri("http://api.icndb.com/jokes/random"); //assigned source url to variable endpoint
-                var endpoint2 = new Uri("http://api.icndb.com/jokes/random?limitTo=[nerdy]");
-                var result = source.GetAsync(endpoint).Result; //output the results from the endpoint
-                var result2 = source.GetAsync(endpoint2).Result;
-                var json = result.Content.ReadAsStringAsync().Result; //converted the result to json
-                var json2 = result2.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(json); //prints json script
-                Console.WriteLine();//make a space
-                Console.WriteLine(json2);
-            }
+            int[] values = {21,35,97,364,3212,3125};
 
-        }   
+            int y = 97;
 
-    }                      
+            Console.WriteLine(values(97));
+        }
+    }
 }
+
