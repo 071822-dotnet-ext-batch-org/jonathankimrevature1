83ef1ce6e7cfdfbbaee998b43dedf418b3ab8e14
Jonathan Kim <jonathan241@revature.net>
2022-09-09 07:17:05 -0700

    change

---
+++ demos/HTML Demos/JavaScript/jsDemo.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+alert('Hacked!!');
+
+//var is the old way of declaring objects
+var var1;
+//let is the newer and more common way
+var var2 = 6;
+
+console.log(var2);
+
+var2 = "this is now a string"
+console.log(`i wrote...${var2}`)
+
+let mystring = 'this is a string';
+console.log(mystring);
+mystring = 1034;
+console.log(mystring);
